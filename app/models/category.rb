e5c46493e953673aa2f54518eb5e2e7310663969
banafederico <federico@neo.com>
2013-10-22 15:59:57 -0200

    Fix syntax error.

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,7 @@
 class Category < ActiveRecord::Base
 
   belongs_to :parent,
-             :foreign_key => :parent_id
+             :foreign_key => :parent_id,
              :class_name => "Category"
 
   has_many :items
