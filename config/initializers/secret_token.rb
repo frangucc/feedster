6c7f3988954d46c7e3e0cafe86575415a2d008af
banafederico <federico@neo.com>
2013-10-22 15:39:47 -0200

    Rails app with postgresql.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Feedster::Application.config.secret_key_base = '717b948a96f38a9a29510492927b7b210bfcfcfcf9d41f9bf06b12292dbb3b9910e1b5a77a8acf5c1ab3382aaec5b2c69827f7a9761614fc3fea65f995b95531'
