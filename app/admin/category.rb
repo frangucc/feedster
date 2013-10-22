664eeb2a08ce89244252dbb3f5ec17f2fd983f71
banafederico <federico@neo.com>
2013-10-22 16:14:03 -0200

    Strong parameters config for active admin resources.

--- app/admin/category.rb
+++ app/admin/category.rb
@@ -1,3 +1,9 @@
 ActiveAdmin.register Category do
 
+  controller do
+    def permitted_params
+      params.permit(:category => [:name, :parent_id])
+    end
+  end
+
 end
--- app/admin/item.rb
+++ app/admin/item.rb
@@ -1,3 +1,9 @@
 ActiveAdmin.register Item do
 
+  controller do
+    def permitted_params
+      params.permit(:item => [:category_id, :title, :content])
+    end
+  end
+
 end
