e105edaf0921bceae15a4dc0d9bab651c48ced5e
Federico Bana <me@banafederico.com>
2013-10-24 22:43:20 -0200

    Add category_id to App.

--- app/admin/app.rb
+++ app/admin/app.rb
@@ -6,4 +6,20 @@ ActiveAdmin.register App do
     end
   end
 
+  form do |f|
+    f.inputs do
+      f.input :name
+      f.input :parent_category
+    end
+    f.actions
+  end
+
+  show do
+    attributes_table do
+      row :id
+      row :name
+      row :parent_category
+    end
+  end
+
 end
--- app/models/app.rb
+++ app/models/app.rb
@@ -5,4 +5,8 @@ class App < ActiveRecord::Base
   has_many :categories,
            :through => :app_parent_categories
 
+  belongs_to :parent_category,
+             :foreign_key => :category_id,
+             :class_name => 'Category'
+
 end
